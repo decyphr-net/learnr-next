9acaa745200034ee18efbd4fe6333c9d0c77a86d
Aaron Sinnott <aaronsnig@gmail.com>
2020-09-06 19:55:26 -0300

    Implement Auth0

--- next.config.js
+++ next.config.js
@@ -7,4 +7,11 @@ module.exports = {
   publicRuntimeConfig: {
     localeSubpaths,
   },
+
+  env: {
+    API: process.env.API,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_MANAGEMENT_API_ENDPOINT: process.env.AUTH0_MANAGEMENT_API_ENDPOINT,
+    AUTH0_MANAGEMENT_API_TOKEN: process.env.AUTH0_MANAGEMENT_API_TOKEN,
+  },
 };
