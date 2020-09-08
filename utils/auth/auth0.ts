a7ca1695fd7886e5ba1cb5cb0bf8d8533e16c626
Aaron Sinnott <aaronsnig@gmail.com>
2020-09-08 16:01:56 -0300

    Update cookie settings for Auth0

--- utils/auth/auth0.ts
+++ utils/auth/auth0.ts
@@ -4,11 +4,15 @@ export default initAuth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  audience: process.env.AUTH0_AUDIENCE,
   scope: process.env.AUTH0_SCOPE,
   redirectUri: process.env.AUTH0_REDIRECT_URI,
   postLogoutRedirectUri: process.env.AUTH0_POST_LOGOUT_REDIRECT,
   session: {
     cookieSecret: process.env.COOKIE_SECRET,
-    cookieLifetime: 60 * 60 * 8,
+    cookieLifetime: 86400 * 30,
+    storeIdToken: false,
+    storeRefreshToken: true,
+    storeAccessToken: true,
   },
 });
