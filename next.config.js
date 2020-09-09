6b9b3563a23c0444677143a4ec2fcd79c97ee605
Aaron Sinnott <aaronsnig@gmail.com>
2020-09-09 16:55:23 -0300

    Add netlify functions for API

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { nextI18NextRewrites } = require("next-i18next/rewrites");
 const localeSubpaths = {};
 
 module.exports = {
+  target: "serverless",
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
