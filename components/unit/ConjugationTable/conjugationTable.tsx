9136c7a20115278853452451b5cb950a67507e24
Aaron Sinnott <aaronsnig@gmail.com>
2020-09-14 12:08:20 -0300

    Update Conjugation Table component

--- components/unit/ConjugationTable/conjugationTable.tsx
+++ components/unit/ConjugationTable/conjugationTable.tsx
@@ -1,5 +1,73 @@
-import { Table } from "../../elements/Table";
+import { Text } from "@chakra-ui/core";
+import {
+  Table,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableBody,
+  TableCell,
+} from "../../elements/Table/Table";
 
-const ConjugationTable = () => <Table></Table>;
+const ConjugationTable = () => (
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableHeader>Pronoun</TableHeader>
+        <TableHeader>Conjugation</TableHeader>
+        <TableHeader />
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      <TableRow bg="white">
+        <TableCell>
+          <Text fontSize="sm">I</Text>
+        </TableCell>
+        <TableCell>
+          <Text fontSize="sm">am</Text>
+        </TableCell>
+      </TableRow>
+      <TableRow bg="gray.50">
+        <TableCell>
+          <Text fontSize="sm">You</Text>
+        </TableCell>
+        <TableCell>
+          <Text>Are</Text>
+        </TableCell>
+      </TableRow>
+      <TableRow bg="white">
+        <TableCell>
+          <Text fontSize="sm">He/She/It</Text>
+        </TableCell>
+        <TableCell>
+          <Text fontSize="sm">Is</Text>
+        </TableCell>
+      </TableRow>
+      <TableRow bg="gray.50">
+        <TableCell>
+          <Text fontSize="sm">We</Text>
+        </TableCell>
+        <TableCell>
+          <Text fontSize="sm">Are</Text>
+        </TableCell>
+      </TableRow>
+      <TableRow bg="white">
+        <TableCell>
+          <Text>You</Text>
+        </TableCell>
+        <TableCell>
+          <Text fontSize="sm">Are</Text>
+        </TableCell>
+      </TableRow>
+      <TableRow bg="gray.50">
+        <TableCell>
+          <Text fontSize="sm">They</Text>
+        </TableCell>
+        <TableCell>
+          <Text fontSize="sm">Are</Text>
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+);
 
 export default ConjugationTable;
